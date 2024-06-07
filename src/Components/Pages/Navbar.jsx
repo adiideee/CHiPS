38617136be54fed125cbce45005273b1d7c8f6b9
Mohit Khedwal <136912167+MohitKhedwal@users.noreply.github.com>
2024-06-07 11:48:28 +0530

    done 10

--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -14,16 +14,16 @@ function Navbar() {
     <nav className="p-4 min-h-4 pb-2 flex justify-between items-center px-1 text-sm font-semibold bg-gray-300">
       <a href='#'> 
         <div className="flex items-center ">
-        <div className="h-10 w-32 mr-2 mb-2 bg-cover bg-no-repeat bg-center navlogo" alt="CG-Govt-Logo" >
+        <div className="h-10 w-32 ml-3 mb-3 pl-2 bg-cover bg-no-repeat bg-center navlogo" alt="CG-Govt-Logo" >
         <img src={logo} alt="" className='w-[80%] ' />
         </div>
           <div className="flex flex-col space-y-1  ml-8">
-            <span className="text-lg font-semibold  mr-2 mb-0 text-black  "> Chhattisgarh Center of Geo-Information (CGCOG)</span>
+            <span className="text-2xl font-semibold  mr-2 mb-0 text-black  "> Chhattisgarh Center of Geo-Information (CGCOG)</span>
             {/* <span className="  text-sm mt-0 text-[#FF00FF]"></span> */}
           </div>
         </div>
         </a>
-      <ul className=" font-semibold flex space-x-4">
+      <ul className=" font-semibold flex  space-x-5 mr-3">
         <li>
          <NavLink to="/" className="flex items-center hover:text-gray-500" > <IoHome className="mr-2 text-lg" />Home</NavLink>
          {/* <a href="#" className="flex items-center hover:text-[#FF00FF]" >
@@ -58,7 +58,7 @@ function Navbar() {
 
             </NavLink>
           {/* <a href="#" className="flex items-center hover:text-[#FF00FF]">
-            <FaDatabase className="mr-2 text-lg" />
+            <FaDatalg className="mr-2 text-lg" />
             Data
           </a> */}
         </li>
