6845a8c2a8c81007bb0c3879e7bd05adea601724
AshfaqueBeig <ashfaquebeig001@gmail.com>
2024-06-28 16:12:10 +0530

    Update Navbar.jsx

--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -19,77 +19,77 @@ function Navbar() {
         </div>
           <div className="flex flex-col space-y-1  ml-16">
             <span className="text-2xl font-semibold  mr-10 mb-0 text-black  "> Chhattisgarh Center of Geo-Information (CGCOG)</span>
-            {/* <span className="  text-sm mt-0 text-[#FF00FF]"></span> */}
+            {/* <span className="  text-sm mt-0 text-[#00000]"></span> */}
           </div>
         </div>
         </a>
       <ul className=" font-semibold flex space-x-4">
         <li>
-         <NavLink to="/" className="flex items-center hover:text-[#FF00FF]" > <IoHome className="mr-2 text-lg" />Home</NavLink>
-         {/* <a href="#" className="flex items-center hover:text-[#FF00FF]" >
+         <NavLink to="/" className="flex items-center hover:text-[#00000]" > <IoHome className="mr-2 text-lg" />Home</NavLink>
+         {/* <a href="#" className="flex items-center hover:text-[#00000]" >
             <AiFillHome className="mr-2 text-lg" />
             Home
           </a> */}
         </li>
         <li>
-            <NavLink to="/about-us" className="flex items-center hover:text-[#FF00FF]" >
+            <NavLink to="/about-us" className="flex items-center hover:text-[#00000]" >
             <GrCircleInformation className="mr-2 text-lg" />
             About Us
 
             </NavLink>
-          {/* <a href="#" className="flex items-center hover:text-[#FF00FF]">
+          {/* <a href="#" className="flex items-center hover:text-[#00000]">
             <IoIosInformationCircle className="mr-2 text-lg" />
             About Us
           </a> */}
         </li>
         <li>
-            <NavLink className="flex items-center hover:text-[#FF00FF]" to="/departments" >
+            <NavLink className="flex items-center hover:text-[#00000]" to="/departments" >
               <FaBuilding className="mr-2 text-lg" color='red-900' />
             Departments</NavLink>
-          {/* <a href="#" className="flex items-center hover:text-[#FF00FF]">
+          {/* <a href="#" className="flex items-center hover:text-[#00000]">
             <PiNotePencilFill  className="mr-2 text-lg" />
             Projects
           </a> */}
         </li>
         <li>
-            <NavLink  className="flex items-center hover:text-[#FF00FF]" to="/data" >
+            <NavLink  className="flex items-center hover:text-[#00000]" to="/data" >
             <AiFillDatabase className="mr-2 text-lg" />
             Data
 
             </NavLink>
-          {/* <a href="#" className="flex items-center hover:text-[#FF00FF]">
+          {/* <a href="#" className="flex items-center hover:text-[#00000]">
             <FaDatabase className="mr-2 text-lg" />
             Data
           </a> */}
         </li>
         <li>
-          <NavLink className="flex items-center hover:text-[#FF00FF]" to="/stategis">
+          <NavLink className="flex items-center hover:text-[#00000]" to="/stategis">
           <IoMap  className="mr-2 text-lg" />
             CG GIS
           </NavLink>
 
-          {/* <a href="#" className="flex items-center hover:text-[#FF00FF]">
+          {/* <a href="#" className="flex items-center hover:text-[#00000]">
             <IoLocationSharp className="mr-2 text-lg" />
             CG GIS
           </a> */}
         </li>
         <li>
-            <NavLink className="flex items-center hover:text-[#FF00FF]" to="/districtgis" >
+            <NavLink className="flex items-center hover:text-[#00000]" to="/districtgis" >
             <FaMapPin  className="mr-2 text-lg" />
             District GIS
             </NavLink>
-          {/* <a href="#" className="flex items-center hover:text-[#FF00FF]">
+          {/* <a href="#" className="flex items-center hover:text-[#00000]">
             <IoLocationSharp className="mr-2 text-lg" />
             District GIS
           </a> */}
         </li>
         <li>
-            <NavLink className="flex items-center hover:text-[#FF00FF]" to="/cgatlas" >
+            <NavLink className="flex items-center hover:text-[#00000]" to="/cgatlas" >
             <FaAtlas className="mr-2 text-lg" />
             CG Atlas
 
             </NavLink>
-          {/* <a href="#" className="flex items-center hover:text-[#FF00FF]">
+          {/* <a href="#" className="flex items-center hover:text-[#00000]">
             <MdContactPhone className="mr-2 text-lg" />
             Contact Us
           </a> */}
