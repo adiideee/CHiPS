c2782bf8d020ff9b46a3cdcc730ce5ac0d37da8e
AshfaqueBeig <ashfaquebeig001@gmail.com>
2024-07-05 11:50:30 +0530

    Update Data.jsx

--- src/Components/Pages/Data.jsx
+++ src/Components/Pages/Data.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Data() {
   return (
-    <div>
+    <div className='{styles.nunitoFont}'>
       <h1 className='text-center font-bold'>THIS IS DATA PAGE</h1>
     </div>
   )
