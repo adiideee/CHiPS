d7c73ec184869f6d65afdfc5f3c9fb6f2adb667d
AshfaqueBeig <ashfaquebeig001@gmail.com>
2024-07-05 11:51:13 +0530

    Update Stategis.jsx

--- src/Components/Pages/Stategis.jsx
+++ src/Components/Pages/Stategis.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Stategis() {
   return (
-    <div>
+    <div className='{styles.nunitoFont}'>
       <h1 className='text-center font-bold'>THIS IS STATE GIS PORTAL</h1>
     </div>
   )
