c8ce3f60e8ee6bd4e639a431c4bd76c1c207b0ff
AshfaqueBeig <ashfaquebeig001@gmail.com>
2024-07-10 10:46:42 +0530

    Update Home.jsx

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Sliderr from '../Container/Sliderr'
-import Card from "../Container/Card"
-import Footer from './Footer'
+import React from "react";
+import Sliderr from "../Container/Sliderr";
+import Card from "../Container/Card";
+import Footer from "./Footer";
 
 function Home() {
-
-
   return (
+<<<<<<< Updated upstream
     <div className='w-full {styles.nunitoFont} h-screen '>
       <Sliderr/>
 <<<<<<< HEAD
@@ -31,57 +30,121 @@ function Home() {
                 their uses has competing and conflicts interests that may arise between the various stakeholders makes compulsory to 
                 enable planning at multiple levels.
                 Government of Chhattisgarh through its nodal agency, Chhattisgarh InfoTech & biotech Promotion Society (CH𝓲PS) an autonomous organization under theDepartment of Information Technology, in consultation with Department of Panchyat and Rural Development (P&RD), and Department of Land Revenue (LRD), has generated Natural Resources Database (NRD), Infrastructure Resource Database and Cadastral Database for the State of Chhattisgarh on various scale from 1:50000 scale for natural resource, 1:4000 for rural cadastral maps to 1:1000 for urban amenities using IRS LISS-III/IV and CARTOSET data made available by NRSA(ISRO). These databases of Natural Resources (NRD), Socio-economics, Infrastructure and other collateral information has helped in project planning, implementation and impact assessment. The land parcel layer in this vector data base has been integrated with the tenancy records of B-1 and Khasra. The delivery of these maps and land records has been made possible in the state for the first time. The secured printing of maps and handling of vector required for capturing mutation data has been done for the first time in the country.
+=======
+    <div className="w-full h-screen ">
+      <Sliderr />
+      <div class="container mx-auto">
+        <section className="mt-3 mb-3 w-[100%] mx-auto  ">
+          <div className="w-[100%] justify-between lg:6xl mx-auto rounded-xl  hover:shadow-[#000000] shadow-lg p-5   ">
+            <h1 className="text-center font-bold text-orange-500 text-4xl">
+              Chhattisgarh GEO Serivces{" "}
+            </h1>
+            <div>
+              <ul className=" text-2xl">
+                <li>
+                  {" "}
+                  <b>Why You Should Visit CH𝓲PS GIS!</b>
+                </li>
+              </ul>
+              <p class="mb-4" style={{ textAlign: "justify" }}>
+                The newly formed State of Chhattisgarh is endowed with natural
+                resources like forest, mineral resources, flora and fauna, wide
+                range of crops and cropping patterns, and rich cultural
+                diversity. The population of Chhattisgarh is divided in{" "}
+                <b>27 districts with 149 tehsils and 20379 revenue villages </b>
+                . The multiplicity of natural resources, their uses has
+                competing and conflicts interests that may arise between the
+                various stakeholders makes compulsory to enable planning at
+                multiple levels. Government of Chhattisgarh through its nodal
+                agency, Chhattisgarh InfoTech & biotech Promotion Society
+                (CH𝓲PS) an autonomous organization under theDepartment of
+                Information Technology, in consultation with Department of
+                Panchyat and Rural Development (P&RD), and Department of Land
+                Revenue (LRD), has generated Natural Resources Database (NRD),
+                Infrastructure Resource Database and Cadastral Database for the
+                State of Chhattisgarh on various scale from 1:50000 scale for
+                natural resource, 1:4000 for rural cadastral maps to 1:1000 for
+                urban amenities using IRS LISS-III/IV and CARTOSET data made
+                available by NRSA(ISRO). These databases of Natural Resources
+                (NRD), Socio-economics, Infrastructure and other collateral
+                information has helped in project planning, implementation and
+                impact assessment. The land parcel layer in this vector data
+                base has been integrated with the tenancy records of B-1 and
+                Khasra. The delivery of these maps and land records has been
+                made possible in the state for the first time. The secured
+                printing of maps and handling of vector required for capturing
+                mutation data has been done for the first time in the country.
+>>>>>>> Stashed changes
               </p>
+            </div>
+            <div class="flex space-x-6 ">
+              <div class="relative flex flex-col mt-6 text-black-700 bg-white shadow-md bg-clip-border shadow-[#000000] rounded-xl w-96">
+                <div class="p-6">
+                  <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                    Development of Spatial Database for Road and railway Network
+                    of Chhattisgarh State
+                  </h5>
+                  <p class="mb-4" style={{ textAlign: "justify" }}>
+                    Digitization of road map provided by PWD/RES, Updation of
+                    road network from high resolution IRS satellite data on
+                    1:50,000 scale and feature extraction from satellite image
+                    for new roads, on 1:50,000 scale. Validation of database in
+                    association with PWD/RES and the digital database generated
+                    in standard GIS format.
+                  </p>
+                </div>
+              </div>
+              <div class="relative flex flex-col mt-6 text-black-700 bg-white shadow-md bg-clip-border shadow-[#000000] rounded-xl w-96">
+                <div class="p-6">
+                  <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                    Natural Resources Database for the state of Chhattisgarh
+                  </h5>
+                  <p class="mb-4" style={{ textAlign: "justify" }}>
+                    Generation of thematic layers on 1:50,000 scale as per NRIS
+                    standards for the entire state viz. Drainage and Watershed,
+                    Surface Water body, Slope and Aspect, Soils, Mineral
+                    Resources, Land use/Land cover, Wasteland map, Transport
+                    Network, Settlement Location and Village Boundary, Rainfall
+                    and Climate (Data from IMD), Socio Economic Data Base as per
+                    NRIS, Census data and other socio economic data. The digital
+                    database generated was as per the standards confirming NRIS
+                    codification and also confirming to standard GIS format. The
+                    layers of lithology, structure, geomorphology and
+                    groundwater potential were also integrated with the database
+                    and made available to Drinking Water Mission.
+                  </p>
+                </div>
+              </div>
+              <div class="relative flex flex-col mt-6 text-black-700 bg-white shadow-md bg-clip-border shadow-[#000000] rounded-xl w-96">
+                <div class="p-6">
+                  <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                    Geo-referencing of Village maps
+                  </h5>
+                  <p class="mb-4" style={{ textAlign: "justify" }}>
+                    Input maps were taken from land record department, and
+                    scanning, digitization and registration of cadastral maps,
+                    submission of check plots for quality check to LRD,
+                    geo-referencing of cadastral maps, submission of sheet-wise
+                    plots on 1:4000 scale, generation of tehsil-wise/revenue
+                    circle-wise mosaic of cadastral maps. This was subsequently
+                    overlaid on Satellite data indented covering the entire
+                    study area from National Data Centre, NRSA, Hyderabad.
+                    Geo-referencing of satellite data, Generation of enhanced
+                    color composites using IRS PAN & LISS-III data was done to
+                    identify geographic details up to parcel level.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <br />
+        <br />
       </div>
-      <div class="flex space-x-6 ">
-  <div class="relative flex flex-col mt-6 text-black-700 bg-white shadow-md bg-clip-border shadow-[#000000] rounded-xl w-96">
-    <div class="p-6">
-      <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-      Development of Spatial Database for Road and railway Network of Chhattisgarh State
-      </h5>
-      <p class="mb-4" style={{textAlign: 'justify'}}>
-      Digitization of road map provided by PWD/RES, Updation of road network from high resolution IRS satellite data on 1:50,000 scale and feature extraction from satellite image for new roads, on 1:50,000 scale. Validation of database in association with PWD/RES and the digital database generated in standard GIS format.
-      </p>
+      <Footer />
     </div>
-
-  </div>
-  <div class="relative flex flex-col mt-6 text-black-700 bg-white shadow-md bg-clip-border shadow-[#000000] rounded-xl w-96">
-    <div class="p-6">
-      <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-      Natural Resources Database for the state of Chhattisgarh
-      </h5>
-      <p class="mb-4" style={{textAlign: 'justify'}}>
-      Generation of thematic layers on 1:50,000 scale as per NRIS standards for the entire state viz. Drainage and Watershed, Surface Water body, Slope and Aspect, Soils, Mineral Resources, Land use/Land cover, Wasteland map, Transport Network, Settlement Location and Village Boundary, Rainfall and Climate (Data from IMD), Socio Economic Data Base as per NRIS, Census data and other socio economic data. The digital database generated was as per the standards confirming NRIS codification and also confirming to standard GIS format. The layers of lithology, structure, geomorphology and groundwater 
-      potential were also integrated with the database and made available to Drinking Water Mission.
-      </p>
-    </div>
-
-  </div>
-  <div class="relative flex flex-col mt-6 text-black-700 bg-white shadow-md bg-clip-border shadow-[#000000] rounded-xl w-96">
-    <div class="p-6">
-      <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-      Geo-referencing of Village maps
-      </h5>
-      <p class="mb-4" style={{textAlign: 'justify'}}>
-      Input maps were taken from land record department, and scanning, digitization and registration of cadastral maps, submission of check plots for quality check to LRD, geo-referencing of cadastral maps, submission of sheet-wise plots on 1:4000 scale, generation of tehsil-wise/revenue circle-wise mosaic of cadastral maps. This was subsequently overlaid on Satellite data indented covering the entire study area from National Data Centre, NRSA, Hyderabad. Geo-referencing of satellite data, Generation of enhanced color composites using IRS PAN & LISS-III data was done to identify geographic details up to parcel level.
-      </p>
-    </div>
-  </div>
-</div>
-
-
-      </div>
-     </section>
-    {/* <Footer/> */}
-    <br />
-    <br />
-
-    {/* <div class="box-border md:box-content">
-kskskskj
-</div> */}
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
