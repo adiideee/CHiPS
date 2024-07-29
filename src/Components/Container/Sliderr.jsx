036427a378d81412becb5a038143b4bf291e154c
ADITI SHARMA <aditisharma98574@gmail.com>
2024-07-29 12:26:38 +0530

    slider updated

--- src/Components/Container/Sliderr.jsx
+++ src/Components/Container/Sliderr.jsx
@@ -1,21 +1,115 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import { FaArrowAltCircleRight } from "react-icons/fa";
-import { FaArrowAltCircleLeft } from "react-icons/fa";
-import { RxDotFilled } from "react-icons/rx";
+// import React, { useState, useEffect, useRef } from 'react';
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+// import { FaArrowAltCircleRight } from "react-icons/fa";
+// import { FaArrowAltCircleLeft } from "react-icons/fa";
+// import { RxDotFilled } from "react-icons/rx";
 
-function Sliderr() { 
+// function Sliderr() { 
+//   const slides = [
+//     { url: "https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider1.png" },
+//     { url: "https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider2.png" },
+//     { url: "https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider3.png" },
+//     { url: "https://meetmyholiders.com/wp-content/uploads/2019/08/chhattisgarh-banner.jpg" },
+//   ];
+
+//   const [currentSlide, setCurrentSlide] = useState(0);
+//   const intervalRef = useRef(null); // 
+
+//   const autoplayInterval = 3000; 
+//   const [isAutoplayEnabled, setIsAutoplayEnabled] = useState(true);
+
+//   useEffect(() => {
+//     if (isAutoplayEnabled) {
+//       intervalRef.current = setInterval(() => {
+//         const newIndex = (currentSlide + 1) % slides.length;
+//         setCurrentSlide(newIndex);
+//       }, autoplayInterval);
+//     }
+
+//     // Ensure interval is cleared on component unmount
+//     return () => clearInterval(intervalRef.current);
+//   }, [currentSlide, autoplayInterval, isAutoplayEnabled]); // Updated dependencies
+
+//   const prevSlide = () => {
+//     const isFirstSlide = currentSlide === 0;
+//     const newIndex = isFirstSlide ? slides.length - 1 : currentSlide - 1;
+//     setCurrentSlide(newIndex);
+
+//     // Optional: Pause autoplay on manual slide change
+//     if (isAutoplayEnabled) {
+//       clearInterval(intervalRef.current);
+//     }
+//   };
+
+//   const nextSlide = () => {
+//     const isLastSlide = currentSlide === slides.length - 1;
+//     const newIndex = isLastSlide ? 0 : currentSlide + 1;
+//     setCurrentSlide(newIndex);
+
+//     // Optional: Pause autoplay on manual slide change
+//     if (isAutoplayEnabled) {
+//       clearInterval(intervalRef.current);
+//     }
+//   };
+
+//   const goToSlide = (slideIndex) => {
+//     setCurrentSlide(slideIndex);
+
+//     // Optional: Pause autoplay on manual slide change
+//     if (isAutoplayEnabled) {
+//       clearInterval(intervalRef.current);
+//     }
+//   };
+
+//   return (
+//     <div className="w-full sm:h-48 md:h-72 lg:h-[500px] overflow-hidden">
+//       <div
+//         style={{ backgroundImage: `url(${slides[currentSlide].url})` }}
+//         className="w-full h-full bg-center bg-cover duration-200 relative "
+//       >
+//         <span
+//           className="absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer   "
+//           onClick={nextSlide}
+//         >
+//           <FaArrowAltCircleRight size={45}  />
+//         </span>
+//         <span
+//           className="absolute left-5 top-1/2 -translate-y-1/2 cursor-pointer "
+//           onClick={prevSlide}
+//         >
+       
+//           <FaArrowAltCircleLeft size={45}  />
+//         </span>
+//         {/* Navigation dots */}
+     
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Sliderr;
+
+import React, { useState, useEffect, useRef } from 'react';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+
+// Import your local images
+import image1 from '../../images/sl1.jpg';
+import image2 from '../../images/slider1.png';
+import image3 from '../../images/slider2.png';
+import image4 from '../../images/slider3.png';
+
+function Sliderr() {
   const slides = [
-    { url: "https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider1.png" },
-    { url: "https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider2.png" },
-    { url: "https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider3.png" },
-    { url: "https://meetmyholiders.com/wp-content/uploads/2019/08/chhattisgarh-banner.jpg" },
+    { url: image1 },
+    { url: image2 },
+    { url: image3 },
+    { url: image4 },
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
-  const intervalRef = useRef(null); // 
+  const intervalRef = useRef(null);
 
-  const autoplayInterval = 3000; 
+  const autoplayInterval = 3000;
   const [isAutoplayEnabled, setIsAutoplayEnabled] = useState(true);
 
   useEffect(() => {
@@ -26,16 +120,14 @@ function Sliderr() {
       }, autoplayInterval);
     }
 
-    // Ensure interval is cleared on component unmount
     return () => clearInterval(intervalRef.current);
-  }, [currentSlide, autoplayInterval, isAutoplayEnabled]); // Updated dependencies
+  }, [currentSlide, autoplayInterval, isAutoplayEnabled]);
 
   const prevSlide = () => {
     const isFirstSlide = currentSlide === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentSlide - 1;
     setCurrentSlide(newIndex);
 
-    // Optional: Pause autoplay on manual slide change
     if (isAutoplayEnabled) {
       clearInterval(intervalRef.current);
     }
@@ -46,7 +138,6 @@ function Sliderr() {
     const newIndex = isLastSlide ? 0 : currentSlide + 1;
     setCurrentSlide(newIndex);
 
-    // Optional: Pause autoplay on manual slide change
     if (isAutoplayEnabled) {
       clearInterval(intervalRef.current);
     }
@@ -55,7 +146,6 @@ function Sliderr() {
   const goToSlide = (slideIndex) => {
     setCurrentSlide(slideIndex);
 
-    // Optional: Pause autoplay on manual slide change
     if (isAutoplayEnabled) {
       clearInterval(intervalRef.current);
     }
@@ -65,123 +155,23 @@ function Sliderr() {
     <div className="w-full sm:h-48 md:h-72 lg:h-[500px] overflow-hidden">
       <div
         style={{ backgroundImage: `url(${slides[currentSlide].url})` }}
-        className="w-full h-full bg-center bg-cover duration-200 relative "
+        className="w-full h-full bg-center bg-cover duration-200 relative"
       >
         <span
-          className="absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer   "
+          className="absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer"
           onClick={nextSlide}
         >
-          <FaArrowAltCircleRight size={45}  />
+          <FaArrowAltCircleRight size={45} />
         </span>
         <span
-          className="absolute left-5 top-1/2 -translate-y-1/2 cursor-pointer "
+          className="absolute left-5 top-1/2 -translate-y-1/2 cursor-pointer"
           onClick={prevSlide}
         >
-       
-          <FaArrowAltCircleLeft size={45}  />
+          <FaArrowAltCircleLeft size={45} />
         </span>
-        {/* Navigation dots */}
-     
       </div>
     </div>
   );
 }
 
 export default Sliderr;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react'
-// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-// import { RxDotFilled } from "react-icons/rx";
-
-
-// function Sliderr() {
-//     const slides=[
-//         {
-//             url:"https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider1.png"
-//         },
-//         {
-//           url:"https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider2.png"
-//         },
-//         {
-//           url:"https://stategisportal.nic.in/stategisportal/assets/img/slider/22/slider3.png"
-//         }
-//     ]
-//     const[currslide,setcurrslide]=useState(0);
-
-//     const prev=()=>{
-//       const first= currslide===0;
-//       const prevdone=first ?slides.length-1 :currslide-1;
-
-//       setcurrslide(prevdone);
-//     }
-
-//     const next=()=>{
-//       const last=currslide===slides.length-1;
-//       const nextdone=last?0:currslide+1;
-
-//       setcurrslide(nextdone)
-//     }
-//     const gotoslide=(index)=>{
-//       setcurrslide(index)
-//     }
-//     setTimeout(next,7000);
-    
-//   return (
-//     <div className='w-full   sm:h-48 md:h-72  lg:h-[500px]  overflow-x-hidden   '>
-//     <div style={{backgroundImage:`url(${slides[currslide].url})`}}  className=' w-full h-full bg-center bg-cover duration-200 relative  '>
-//     <span className='relative left-[95%] top-[50%] cursor-pointer   ' onClick={next}>
-//        <FaArrowRight  size={25} color='#a3b8c2'/>
-//        </span>
-//        <span className='relative left-[5%] top-[50%]  cursor-pointer ' onClick={prev}>
-//        <FaArrowLeft  size={25} color='#a3b8c2'/>
-//        </span>
-//        {/* <div className='absolute right-1/2 top-'></div> */}
-//        {/* <div className='relative flex cursor-pointer justify-center top-5'>
-//         {/* {
-//           slides.map((slide,index)=>(
-//             <div onClick={gotoslide(index)} key={index}>
-
-//               <RxDotFilled size={30} />
-//             </div>  
-//           ))
-//         } */}
-//        {/* </div>  */}
-//      </div>
-
-//   </div>
-//   )
-// }
-
-// export default Sliderr
